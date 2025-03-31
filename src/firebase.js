aa74bead32600e89748b1a614408457d6c73871c
Insy Hernsvan <insy.hernsvan@envisionpharma.com>
2025-03-31 14:35:33 -0400

    update firebase with latest config

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,14 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: "insta-next-v2.firebaseapp.com",
-  projectId: "insta-next-v2",
-  storageBucket: "insta-next-v2.appspot.com",
-  messagingSenderId: "971986010195",
-  appId: "1:971986010195:web:6d48fe4db9a0c1817606a6"
+  authDomain: "insta-next-inz.firebaseapp.com",
+  projectId: "insta-next-inz",
+  storageBucket: "insta-next-inz.firebasestorage.app",
+  messagingSenderId: "755538336338",
+  appId: "1:755538336338:web:56e69169b16c3870d19d8b",
 };
 
 // Initialize Firebase
